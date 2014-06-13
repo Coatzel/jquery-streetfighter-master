5a2ed6340df5543e9f242f04f5fc1c670381e977
Coatzel <coatzel21@gmail.com>
2014-06-13 11:14:01 -0400

    updated version
    updated

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+$('.ryu').mouseenter(function() {
+$(".ryu-still").hide();
+$(".ryu-ready").show();
+})
+.mouseleave(function(){
+	$(".ryu-ready").hide();
+	$(".ryu-still").show();
+})
+.mousedown(function(){
+$(".ryu-ready").hide();
+$(".ryu-throwing").show();
+$(".hadouken").show()
+.animate(
+	{'left': '300px'},
+	500,
+	function() {
+		$(this).hide();
+		$(this).css('left', '-212px');
+		}
+	);
+})
+.mouseup(function(){
+$(".ryu-throwing").hide();
+$(".ryu-ready").show();
+	//ryu goes back to his ready position
+});
+
+
+});
